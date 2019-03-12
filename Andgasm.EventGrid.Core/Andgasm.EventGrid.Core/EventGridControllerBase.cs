c862272a1b76f5bbe5a7a1fa8f6bdf9e38985e85
Danny Brown <machinagod@googlemail.com>
2019-03-12 15:43:40 +0000

    controller base pulled in

---
+++ Andgasm.EventGrid.Core/Andgasm.EventGrid.Core/EventGridControllerBase.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andgasm.API.Core
+{
+    public class EventGridControllerBase : ControllerBase
+    {
+        private bool EventTypeSubcriptionValidation
+            => HttpContext.Request.Headers["aeg-event-type"].FirstOrDefault() ==
+               "SubscriptionValidation";
+
+        private bool EventTypeNotification
+            => HttpContext.Request.Headers["aeg-event-type"].FirstOrDefault() ==
+               "Notification";
+
+        protected ILogger _logger { get; set; }
+
+        public EventGridControllerBase(ILogger<ReportableControllerBase> logger) : base()
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("I am running!");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                var jsonContent = await reader.ReadToEndAsync();
+                if (EventTypeSubcriptionValidation)
+                {
+                    return await HandleValidation(jsonContent);
+                }
+                else if (EventTypeNotification)
+                {
+                    return await HandleEvents(jsonContent);
+                }
+                return BadRequest();
+            }
+        }
+
+        public virtual async Task<IActionResult> HandleEvents(string jsonContent)
+        {
+            await Task.CompletedTask;
+            throw new NotImplementedException("No base implementation exists for handling of event grid messages!");
+        }
+
+        public async Task<JsonResult> HandleValidation(string jsonContent)
+        {
+            var gridEvent = await Task.Run(() => JsonConvert.DeserializeObject<List<GridEvent<Dictionary<string, string>>>>(jsonContent).First());
+            var validationCode = gridEvent.Data["validationCode"];
+            return new JsonResult(new
+            {
+                validationResponse = validationCode
+            });
+        }
+    }
+}
---
+++ Andgasm.EventGrid.Core/Andgasm.EventGrid.Core/GridEvent.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Andgasm.API.Core
+{
+    public class GridEvent<T> where T : class
+    {
+        public string Id { get; set; }
+        public string EventType { get; set; }
+        public string Subject { get; set; }
+        public DateTime EventTime { get; set; }
+        public T Data { get; set; }
+        public string Topic { get; set; }
+    }
+
+    public class CloudEvent<T> where T : class
+    {
+        [JsonProperty("eventID")]
+        public string EventId { get; set; }
+
+        [JsonProperty("cloudEventsVersion")]
+        public string CloudEventVersion { get; set; }
+
+        [JsonProperty("eventType")]
+        public string EventType { get; set; }
+
+        [JsonProperty("eventTypeVersion")]
+        public string EventTypeVersion { get; set; }
+
+        [JsonProperty("source")]
+        public string Source { get; set; }
+
+        [JsonProperty("eventTime")]
+        public string EventTime { get; set; }
+
+        [JsonProperty("data")]
+        public T Data { get; set; }
+    }
+}
